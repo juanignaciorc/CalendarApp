997d0fd42f4fdd99c26ce8498f67503e41111767
Juan Ignacio Roldan <juanignacioroldan01@gmail.com>
2021-02-23 01:16:25 -0300

    react-big-calendar added and styled

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
 import {NavBar} from "../ui/NavBar";
+
+
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+	title: 'Cumpleaños del jefe',
+	start: moment().toDate(),
+	end: moment().add(2, 'hours').toDate(),
+	bgcolor: '#fafafa',
+}]
 
 export const CalendarScreen = () => {
 	return (
-		<NavBar />
+		<div className="calendar-screen">
+			<NavBar />
+
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+			/>
+
+		</div>
+
 	);
 };
 
